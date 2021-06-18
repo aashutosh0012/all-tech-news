28da130c51d187d03f4e83f9243f192568701dc3
Aashutosh Kumar <aashutosh0012@gmail.com>
2021-06-19 00:57:37 +0530

    Update settings.py

--- nagg/settings.py
+++ nagg/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@!5gaschxaam6t#ct!$8zjdhc^h+oiw2j(&d478fkrrgq%p$2i'
+# SECURITY WARNING: keep the secret key used in production secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
